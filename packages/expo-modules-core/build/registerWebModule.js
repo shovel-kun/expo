ac9ff7a3e6c0ff065384963f77216c998f3c11a9
aleqsio <mikucki@gmail.com>
2024-06-18 16:16:09 +0200

    [expo-modules-core] Add web modules to global
    Create class instance
    
    Update

---
+++ packages/expo-modules-core/build/registerWebModule.js
@@ -0,0 +1,6 @@
+// It is a no-op function that returns the module implementation.
+// Actual implementation is located in `createWebModule.web.ts`.
+export function registerWebModule(_moduleName, moduleImplementation) {
+    return moduleImplementation;
+}
+//# sourceMappingURL=registerWebModule.js.map
